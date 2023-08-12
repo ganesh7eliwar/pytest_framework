870ff17b14c1ab8bb853075c734daf6706ba0935
ganesh7eliwar <ganesh7eliwar@gmail.com>
2023-08-12 16:16:54 +0530

    fifth commit

--- running_through_chrome.bat
+++ running_through_chrome.bat
@@ -1,1 +1,1 @@
-pytest -v -s -n=2 --html=HTML_Report/myreport.html --alluredir="D:\Pytest_Framework\CredKart_pytest\allure-report" --browser chrome
+pytest -v -s -n=2 --html=HTML_Report/myreport.html --alluredir="D:\Pytest_Framework\CredKart_pytest\allure-results" --browser chrome
